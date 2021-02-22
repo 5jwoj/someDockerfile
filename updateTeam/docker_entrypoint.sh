8bb453c3a234c7685523206a9e4f231a2856aee8
Aaron-lv <674560606@qq.com>
2021-02-22 12:42:17 +0800

    添加新镜像

---
+++ updateTeam/docker_entrypoint.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+set -e
+
+#获取配置的自定义参数
+if [ $1 ]; then
+    run_cmd=$1
+fi
+
+echo "设定远程仓库地址..."
+cd /scripts
+git remote set-url origin $REPO_URL
+echo "git pull拉取最新代码..."
+git -C /scripts reset --hard
+git -C /scripts pull origin master --rebase
+echo "npm install 安装最新依赖..."
+npm install --loglevel error --prefix /scripts
+
+function initupdateTeam() {
+    mkdir /updateTeam
+    cd /updateTeam
+    git init
+    git remote set-url origin $updateTeam_URL
+    git pull origin master --rebase
+}
+
+if [ ! -d "/updateTeam/" ]; then
+    echo "未检查到updateTeam仓库，初始化下载..."
+    initupdateTeam
+else
+    echo "更新updateTeam仓库文件..."
+    git -C /updateTeam reset --hard
+    git -C /updateTeam pull origin master --rebase
+    git -C /updateTeam push origin master
+fi
+
+echo "------------------------------------------------执行定时任务任务shell脚本------------------------------------------------"
+sh -x /scripts/docker/default_task.sh
+echo "--------------------------------------------------默认定时任务执行完成---------------------------------------------------"
+
+if [ $run_cmd ]; then
+    echo "启动crondtab定时任务主进程..."
+    crond -f
+else
+    echo "默认定时任务执行结束。"
+fi
+echo -e "\n\n"
---
+++ updateTeam/proc_file.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+
+
+##定义合并定时任务相关文件路径变量
+defaultListFile="/jds/updateTeam/$DEFAULT_LIST_FILE"
+customListFile="/jds/updateTeam/$CUSTOM_LIST_FILE"
+mergedListFile="/jds/updateTeam/merged_list_file.sh"
+
+
+##京东京喜工厂自动开团
+if [ $jd_jxFactoryCreateTuan_ENABLE = "Y" ]; then
+    sed -i "s/.\/shareCodes/\/updateTeam/g" /scripts/jd_jxFactoryCreateTuan.js
+    echo "# 京东京喜工厂自动开团" >> /jds/updateTeam/merged_list_file.sh
+    echo "55 */1 * * * node /scripts/jd_jxFactoryCreateTuan.js >> /scripts/logs/jd_jxFactoryCreateTuan.log 2>&1" >> /jds/updateTeam/merged_list_file.sh
+fi
