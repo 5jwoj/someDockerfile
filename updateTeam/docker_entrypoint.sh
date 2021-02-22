8a1b303cc5ca7eb9a948047d29db763aa2606d4c
Aaron-lv <674560606@qq.com>
2021-02-22 16:23:11 +0800

    优化

--- updateTeam/docker_entrypoint.sh
+++ updateTeam/docker_entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -e
 
 #获取配置的自定义参数
 if [ $1 ]; then
@@ -18,31 +19,6 @@ cd /jds
 echo "更新jds仓库文件..."
 git reset --hard
 git pull origin master --rebase
-
-function initupdateTeam() {
-    mkdir /updateTeam
-    cd /updateTeam
-    git init
-    git remote add -f origin $updateTeam_URL
-    git config --global user.email "$email"
-    git config --global user.name "$name"
-    echo -e $KEY > /root/.ssh/id_rsa
-    git pull origin master --rebase
-}
-
-if [ ! -d "/updateTeam/" ]; then
-    echo "未检查到updateTeam仓库，初始化下载..."
-    initupdateTeam
-else
-    cd /updateTeam
-    echo "更新updateTeam仓库文件..."
-    git reset --hard
-    git pull origin master --rebase
-    echo "提交updateTeam仓库文件..."
-    git add -A
-    git commit -m "更新JSON文件"
-    git push origin master
-fi
 
 echo "------------------------------------------------执行定时任务任务shell脚本------------------------------------------------"
 sh -x /jds/updateTeam/default_task.sh
--- updateTeam/proc_file.sh
+++ updateTeam/proc_file.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-set -e
 
 ##定义合并定时任务相关文件路径变量
 defaultListFile="/jds/updateTeam/$DEFAULT_LIST_FILE"
@@ -7,9 +6,40 @@ customListFile="/jds/updateTeam/$CUSTOM_LIST_FILE"
 mergedListFile="/jds/updateTeam/merged_list_file.sh"
 
 
+function initupdateTeam() {
+    mkdir /updateTeam
+    cd /updateTeam
+    git init
+    git remote add -f origin $updateTeam_URL
+    git config --global user.email "$email"
+    git config --global user.name "$name"
+    echo -e $KEY > /root/.ssh/id_rsa
+    git pull origin master --rebase
+}
+
+if [ 0"$updateTeam_URL" = "0" ]; then
+    echo "没有配置远程仓库地址，跳过初始化"
+else
+    if [ ! -d "/updateTeam/" ]; then
+        echo "未检查到updateTeam仓库，初始化下载..."
+        initupdateTeam
+    else
+        cd /updateTeam
+        echo "更新updateTeam仓库文件..."
+        git reset --hard
+        git pull origin master --rebase
+        cp -rf /shareCodes /updateTeam/
+        echo "提交updateTeam仓库文件..."
+        git add -A
+        git commit -m "更新JSON文件"
+        git push origin master
+    fi
+fi
+
+
 ##京东京喜工厂自动开团
 if [ $jd_jxFactoryCreateTuan_ENABLE = "Y" ]; then
-    sed -i "s/.\/shareCodes/\/updateTeam\/shareCodes/g" /scripts/jd_jxFactoryCreateTuan.js
+    sed -i "s/.\/shareCodes/\/shareCodes/g" /scripts/jd_jxFactoryCreateTuan.js
     echo "# 京东京喜工厂自动开团" >> $mergedListFile
     echo "55 */1 * * * node /scripts/jd_jxFactoryCreateTuan.js >> /scripts/logs/jd_jxFactoryCreateTuan.log 2>&1" >> $mergedListFile
 fi
