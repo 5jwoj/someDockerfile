d9313cd7b7dad21f34461e4e7c2b04f622320bbc
Aaron-lv <674560606@qq.com>
2021-03-11 22:12:20 +0800

    修复错误

--- telethon/scripts_update.sh
+++ telethon/scripts_update.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 set -e
 
-MAINPID=`ps -ef|grep jd_zbj|grep -v grep`
+PIDS=`ps -ef |grep jd_zbj |grep -v grep | awk '{print $2}'`
 
 if [ ! -f "/telethon/jd_zbj.py" ]; then
     echo "京东直播间抽奖脚本不存在，跳过执行..."
 else
     echo "京东直播间抽奖脚本存在，判断进程是否存在..."
-    if [ ! "$MAINPID" ];then
+    if [ "$PIDS" != "" ]; then
+        echo "进程存在，跳过执行..."
+    else
         echo "进程不存在，执行脚本..."
         python3 /telethon/jd_zbj.py >> /logs/jd_zbj.log 2>&1
-    else
-        echo "进程存在，跳过执行..."
     fi
 fi
