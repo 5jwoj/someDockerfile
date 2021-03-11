05753c3c8119859870b0b4573b2d857466217024
Aaron-lv <674560606@qq.com>
2021-03-12 01:04:19 +0800

    更新脚本

--- telethon/scripts_update.sh
+++ telethon/scripts_update.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
 set -e
 
-PIDS=`ps -ef |grep jd_zbj |grep -v grep | awk '{print $2}'`
 
 if [ ! -f "/telethon/jd_zbj.py" ]; then
     echo "京东直播间抽奖脚本不存在，跳过执行..."
 else
-    echo "京东直播间抽奖脚本存在，判断进程是否存在..."
-    if [ "$PIDS" != "" ]; then
-        echo "进程存在，跳过执行..."
-    else
-        echo "进程不存在，执行脚本..."
-        python3 -u /telethon/jd_zbj.py |ts >> /logs/jd_zbj.log 2>&1 &
-    fi
+    echo "京东直播间抽奖脚本存在，执行并重启脚本..."
+    eval $(ps -ef | grep "jd_zbj" | grep -v "grep" | awk '{print "kill "$1}')
+    python3 -u /telethon/jd_zbj.py |ts >> /logs/jd_zbj.log 2>&1 &
+    echo "京东直播间抽奖脚本，重启完成..."
 fi
