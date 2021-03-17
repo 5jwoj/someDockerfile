893aca0f8e2c0b259f60dc318c64e3d1ab4e1ff5
Aaron-lv <674560606@qq.com>
2021-03-17 23:23:48 +0800

    更新脚本

--- updateTeam/proc_file.sh
+++ updateTeam/proc_file.sh
@@ -6,13 +6,14 @@ customListFile="/jds/updateTeam/$CUSTOM_LIST_FILE"
 mergedListFile="/jds/updateTeam/merged_list_file.sh"
 
 function initupdateTeam() {
+    git config --global user.email "$email"
+    git config --global user.name "$name"
+    echo -e $KEY > /root/.ssh/id_rsa
+
     mkdir /updateTeam
     cd /updateTeam
     git init
     git remote add -f origin $updateTeam_URL
-    git config --global user.email "$email"
-    git config --global user.name "$name"
-    echo -e $KEY > /root/.ssh/id_rsa
     git pull origin $updateTeam_BRANCH --rebase
 }
 
@@ -30,25 +31,25 @@ else
     fi
 fi
 
-##京东京喜工厂自动开团
+##京喜工厂自动开团
 if [ $jd_jxFactoryCreateTuan_ENABLE = "Y" ]; then
     sed -i "s/.\/shareCodes/\/shareCodes/g" /scripts/jd_jxFactoryCreateTuan.js
-    echo "执行京东京喜工厂自动开团..."
-    node /scripts/jd_jxFactoryCreateTuan.js >> /scripts/logs/jd_jxFactoryCreateTuan.log 2>&1
+    echo "# 京喜工厂自动开团" >> $mergedListFile
+    echo "0 */1 * * * node /scripts/jd_jxFactoryCreateTuan.js >> /scripts/logs/jd_jxFactoryCreateTuan.log 2>&1" >> $mergedListFile
 fi
 
-##更新领现金
+##京东签到领现金
 if [ $jd_updateCash_ENABLE = "Y" ]; then
     sed -i "s/.\/shareCodes/\/shareCodes/g" /scripts/jd_updateCash.js
-    echo "执行更新领现金..."
-    node /scripts/jd_updateCash.js >> /scripts/logs/jd_updateCash.log 2>&1
+    echo "# 京东签到领现金" >> $mergedListFile
+    echo "0 */1 * * * node /scripts/jd_updateCash.js >> /scripts/logs/jd_updateCash.log 2>&1" >> $mergedListFile
 fi
 
 ##赚京豆小程序
 if [ $jd_zzUpdate_ENABLE = "Y" ]; then
     sed -i "s/.\/shareCodes/\/shareCodes/g" /scripts/jd_zzUpdate.js
-    echo "执行赚京豆小程序..."
-    node /scripts/jd_zzUpdate.js >> /scripts/logs/jd_zzUpdate.log 2>&1
+    echo "# 赚京豆小程序" >> $mergedListFile
+    echo "0 */1 * * * node /scripts/jd_zzUpdate.js >> /scripts/logs/jd_zzUpdate.log 2>&1" >> $mergedListFile
 fi
 
 cp -rf /shareCodes /updateTeam/
