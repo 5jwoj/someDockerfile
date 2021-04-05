c57dd8561b209b388aba662e05b38bd1f191bd2a
Aaron-lv <674560606@qq.com>
2021-04-05 17:38:59 +0800

    更新脚本

--- jd_scripts/default_task.sh
+++ jd_scripts/default_task.sh
@@ -220,7 +220,7 @@ echo "第10步加载最新的定时任务文件..."
 if [[ -f /usr/bin/jd_bot && -z "$DISABLE_SPNODE" ]]; then
     echo "bot交互与spnode前置条件成立，替换任务列表的node指令为spnode"
     sed -i "s/ node / spnode /g" $mergedListFile
-    sed -i "/jd_blueCoin.js\|jd_joy_reward.js\|jd_car_exchange.js\|jd_carnivalcity.js/s/spnode/spnode conc/g" $mergedListFile
+    sed -i "/jd_blueCoin.js\|jd_joy_reward.js\|jd_car_exchange.js/s/spnode/spnode conc/g" $mergedListFile
 fi
 crontab $mergedListFile
 
